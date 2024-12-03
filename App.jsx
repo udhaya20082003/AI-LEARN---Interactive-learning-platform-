de5d70ec6db8618351e87a0ab490a3a275194e30
udhaya20082003 <123627902+udhaya20082003@users.noreply.github.com>
2024-12-03 10:55:31 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,102 @@
+import { Route, Routes } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Homepage from "./pages/Homepage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import MyLearningsPage from "./pages/MyLearningsPage";
+import PageNotFound from "./pages/PageNotFound";
+import ShoppingCartPage from "./pages/ShoppingCartPage";
+import ProfilePage from "./pages/ProfilePage";
+import ProfileInfoPage from "./pages/ProfileInfoPage";
+import ProfilePasswordSecurityPage from "./pages/ProfilePasswordSecurityPage";
+import ProfilePrivacyPage from "./pages/ProfilePrivacyPage";
+import ProfileNotificationPage from "./pages/ProfileNotificationPage";
+import ProfileCloseAcountPage from "./pages/ProfileCloseAcountPage";
+import AllCoursesPage from "./pages/AllCoursesPage";
+import InstructorPage from "./pages/Instructor/InstructorPage";
+import Activation from "./components/Registration/Activation/Activation";
+import ResetPassword from "./components/Registration/ResetPassword/ResetPassword";
+import ResetPasswordConfirm from "./components/Registration/ResetPassword/ResetPasswordConfirm";
+import Summarizers from "./pages/Summarizers";
+import ChatBot from "./pages/ChatBot";
+import CoursesContentPage from "./pages/Instructor/CoursesContentPage";
+import RequireAuth from "./components/Registration/RequireAuth/RequireAuth";
+import QuestionGenerationPage from "./pages/QuestionGenerationPage";
+import UserPage from "./pages/Users/UserPage";
+import ContactUs from "./shared/ContactUs";
+import AboutUs from "./shared/AboutUs";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { load_user } from "./redux/actions/auth-methods";
+
+const App = () => {
+  const dispatch = useDispatch();
+  const userAuth = useSelector((state) => state.userAuth);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      if (userAuth.isAuthenticated) {
+        if (!userAuth.user) {
+          await load_user(dispatch);
+        }
+      }
+    };
+    fetchUserData();
+  }, [userAuth, dispatch]);
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Homepage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="signup" element={<SignUpPage />} />
+          <Route path="reset-password" element={<ResetPassword />} />
+          <Route
+            path="password/reset/confirm/:uid/:token"
+            element={<ResetPasswordConfirm />}
+          />
+          <Route path="activate/:uid/:token" element={<Activation />} />
+          <Route path="about-us" element={<AboutUs />} />
+          <Route path="*" element={<PageNotFound />} />
+          <Route element={<RequireAuth />}>
+            <Route path="my-learnings" element={<MyLearningsPage />} />
+            <Route path="cart" element={<ShoppingCartPage />} />
+            <Route path="all-courses" element={<AllCoursesPage />} />
+            <Route path="instructor" element={<InstructorPage />} />
+            <Route path="profile" element={<ProfilePage />}>
+              <Route path="profileInfo" element={<ProfileInfoPage />} />
+              <Route
+                path="password&security"
+                element={<ProfilePasswordSecurityPage />}
+              />
+              <Route path="privacy" element={<ProfilePrivacyPage />} />
+              <Route
+                path="Notification"
+                element={<ProfileNotificationPage />}
+              />
+              <Route
+                path="close-account"
+                element={<ProfileCloseAcountPage />}
+              />
+            </Route>
+            <Route path="summarizer" element={<Summarizers />} />
+            <Route
+              path="questionqeneration"
+              element={<QuestionGenerationPage />}
+            />
+            <Route path="ChatBot/:slug" element={<ChatBot />} />
+            <Route
+              path="CoursesContentPage/:slug"
+              element={<CoursesContentPage />}
+            />
+            <Route path="UserContentPage/:slug" element={<UserPage />} />
+            <Route path="contact-us" element={<ContactUs />} />
+          </Route>
+        </Route>
+      </Routes>
+    </>
+  );
+};
+
+export default App;
